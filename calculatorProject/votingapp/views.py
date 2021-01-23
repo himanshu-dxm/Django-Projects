ce6a69c172da6ee80b09782b4e41a24d88d8a5c3
himanshu <himanhu.behl@gmail.com>
2021-01-24 00:00:12 +0530

    added the list and count of votes

--- calculatorProject/votingapp/views.py
+++ calculatorProject/votingapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
+arr = ['Java','Python','C++','C','DotNet','PHP','JavaScript',
+       'Swift','HTML','CSS','SQL','Ruby','Objectice-C',
+       'Go','VisualBasic','MATLAB','R','Pearl']
+globalcnt = dict()
 
 def index(request):
-    return render(request,'indexV.html')
+    mydict = {
+        "arr":arr
+    }
+    return render(request,'indexV.html',context=mydict)
+
+def getquery(request):
+    q=request.GET['languages']
+    if q in globalcnt:
+        globalcnt[q]+=1
+    else:
+        globalcnt[q]=1
+    
+    mydict = {
+        "q" : q,
+        "globalcnt" : globalcnt
+    }
+    return render(request,'indexV.html',context=mydict)
