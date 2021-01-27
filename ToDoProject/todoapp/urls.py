24cd3e434eb35413735d1075dc2ca325feebe49d
himanshu <himanhu.behl@gmail.com>
2021-01-27 15:26:01 +0530

    added a new url

--- ToDoProject/todoapp/urls.py
+++ ToDoProject/todoapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('todoapp',views.index,name="index"),
     path('submit',views.submit,name="submit"),
     path('delete/<int:id>',views.delete,name="delete"),
     path('list',views.list,name="list"),
